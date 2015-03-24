7c8b72060c922b08685be355090aa6f6430d5857
Michael Becco <e4e6d4d5@gmail.com>
2015-03-23 21:50:28 -0700

    Refactor and Revise implementation

--- fizzbuzz_extended/lib/fizzbuzz_extended.rb
+++ fizzbuzz_extended/lib/fizzbuzz_extended.rb
@@ -1,30 +1,25 @@
 class FizzBuzzExtended
   def initialize(h)
     @hashbuzz = h
-    @hive = []
   end
 
-  def buzzoven(cycles = 50_000)
+  def buzzoven(n)
+    @hive = ''
     @hashbuzz.each do |k, v|
-      (1..cycles).each do |n|
-        if (n % k).zero?
-          @hive << "FizzBuzz#{ v }"
-        elsif (n % 15).zero?
-          @hive << "FizzBuzz"
-        elsif (n % 5).zero?
-          @hive << "Buzz"
-        elsif (n % 3).zero?
-          @hive << "Fizz"
-        else
-          @hive << n
-        end
+      if (n % k).zero?
+        @hive << "#{ v }"
       end
     end
-    @hive
+    @hive.empty? ? n : @hive
   end
 
-  def swarm
-    @hive.each { |e| puts "#{e}\n" }
+  def swarm(cycles = 50_000)
+    (1..cycles).each do |n|
+      puts buzzoven(n)
+    end
   end
 end
 
+h = { 3 => 'Fizz', 5 => 'Buzz', 7 => 'Siiv' }
+@fbe = FizzBuzzExtended.new(h)
+@fbe.swarm(cycles = 21)
